b8c23d2b2fabb8d4d8b9889dd5fb676afa5a0dbf
Jonathan Olsten <jolsten@gmail.com>
2023-09-22 13:43:29 -0400

    tweaks

--- tests/test_flf.py
+++ tests/test_flf.py
@@ -2,10 +2,19 @@ import pathlib
 import pytest
 from sodafile.flf.utils import Section, LabelFile
 from .conftest import sample_files, SampleFile
+from . import strategies as cst
+
+
+@given(cst.unwrapped_sections())
+def test_read_write_section(sample_files: SampleFile):
+    text = sample_files.flf.read_text(encoding="utf-8")
+    sect = Section.from_unwrapped_line(text)
+    output = sect.to_unwrapped_line()
+    assert text == output
 
 
 @pytest.mark.parametrize("sample_file", sample_files)
-def test_parse_flf(sample_file: SampleFile):
+def test_read_flf(sample_file: SampleFile):
     text = sample_file.flf.read_text(encoding="utf-8")
     flf = LabelFile.from_text(text)
     print(flf.sections)
