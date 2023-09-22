ead41e1252217fa1f75c4bc6b843a762dca14d13
Jonathan Olsten <jolsten@gmail.com>
2023-09-21 20:15:07 -0400

    added line_to_dict/and back

---
+++ sodafile/flf/utils.py
@@ -0,0 +1,88 @@
+from dataclasses import dataclass, field
+import re
+from textwrap import TextWrapper
+from typing import Dict, List, Optional
+
+NEW_SECTION = re.compile(r"^(?P<section>\S+\s+)(?P<rest>.*)$")
+
+
+def _line_to_dict(text: str) -> Dict[str, str]:
+    kvpairs = text.strip().split()
+    results = {}
+    for kvp in kvpairs:
+        try:
+            key, val = kvp.split("=")
+        except ValueError:
+            pass
+        results[key] = val
+    return results
+
+
+def _dict_to_line(data: Dict[str, str]) -> str:
+    return " ".join([f"{key}={val}" for key, val in data.items()])
+
+
+def _unwrap_sections(text: str) -> List[str]:
+    out = ""
+    for line in text.splitlines():
+        if match := NEW_SECTION.match(line):
+            sect = match.group("section")
+            rest = match.group("rest").strip()
+            print(len(sect))
+            out += f"\n{sect}{rest}"
+        else:
+            out += " " + line.strip()
+    return out.lstrip().splitlines()
+
+
+def _wrap_sections(sections: List[str], indent: int) -> str:
+    wrapper = TextWrapper(
+        width=79,
+        subsequent_indent=" " * indent,
+        break_on_hyphens=False,
+        break_long_words=False,
+    )
+
+    out = []
+    for text in sections:
+        section, rest = text.split(maxsplit=1)
+        wrapper.initial_indent = f"{section: <{indent}}"
+        wrapped = wrapper.wrap(rest)
+        out.extend(wrapped)
+
+    out = [f"{line:<79}" for line in out]
+    return "\n".join(out) + "\n"
+
+
+@dataclass
+class Section:
+    name: str
+    values: Dict[str, str]
+
+    @classmethod
+    def from_unwrapped_line(cls, text: str) -> "Section":
+        name, rest = text.split(maxsplit=1)
+        values = {}
+        for item in rest.split():
+            try:
+                key, val = item.split("=")
+                values[key] = val
+            except ValueError:
+                pass
+        return cls(name=name, values=values)
+
+    def to_unwrapped_line(self, text: str) -> str:
+        kvpairs = " ".join([f"{key}={val}" for key, val in self.values])
+        return f"{self.name} {kvpairs}"
+
+
+@dataclass
+class LabelFile:
+    sections: List[Section] = field()
+
+    @classmethod
+    def from_text(cls, text: str) -> "LabelFile":
+        pass
+
+    def to_text(self) -> str:
+        return ""
---
+++ tests/strategies.py
@@ -0,0 +1,63 @@
+from typing import List
+from hypothesis import strategies as st, assume
+
+SECTIONS = ["VOLUME", "FILE", "EVENT", "PROCESSOR"]
+INDENT_SIZE = max([len(sect) for sect in SECTIONS]) + 1
+
+
+@st.composite
+def section_names(draw, sections: List[str] = SECTIONS):
+    return draw(st.sampled_from(sections))
+
+
+@st.composite
+def keys(draw, min_size: int = 3, max_size: int = 10):
+    x = draw(
+        st.text(
+            alphabet=st.sampled_from("ABCDEFGHIJKLMNOPQRSTUVWXYZ_"),
+            min_size=min_size,
+            max_size=max_size,
+        )
+    )
+    return x.upper()
+
+
+@st.composite
+def values(draw, min_size: int = 1, max_size: int = 20):
+    x = draw(
+        st.text(
+            alphabet=st.sampled_from("ABCDEFGHIJKLMNOPQRSTUVWXYZ_-,"),
+            min_size=min_size,
+            max_size=max_size,
+        )
+    )
+    return x.upper()
+
+
+@st.composite
+def key_val_pairs(
+    draw,
+    min_key_size: int = 3,
+    max_key_size: int = 10,
+    min_val_size: int = 1,
+    max_val_size: int = 20,
+):
+    key = draw(keys(min_size=min_key_size, max_size=max_key_size)).upper()
+    val = draw(values(min_size=min_val_size, max_size=max_val_size)).upper()
+    kvp = f"{key}={val}"
+    return kvp
+
+
+@st.composite
+def key_val_dict(draw, min_size: int = 1, max_size: int = 10):
+    keys_ = draw(st.lists(keys(), min_size=min_size, max_size=max_size, unique=True))
+    return {key: draw(values()) for key in keys_}
+
+
+@st.composite
+def unwrapped_sections(draw, min_kvp: int = 1, max_kvp: int = 10):
+    section = draw(section_names())
+    kvpairs = draw(st.lists(key_val_pairs(), min_size=min_kvp, max_size=max_kvp))
+    joined = " ".join(kvpairs)
+    unwrapped = f"{section: <{INDENT_SIZE}}{joined}"
+    return unwrapped
