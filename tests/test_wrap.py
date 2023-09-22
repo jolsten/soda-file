c3980e80629ed466e9ecfdafa2fb702cb833d686
Jonathan Olsten <jolsten@gmail.com>
2023-09-21 20:47:15 -0400

    more things

--- tests/test_wrap.py
+++ tests/test_wrap.py
@@ -1,21 +1,49 @@
 import pytest
-from sodafile.flf.generic import _wrap_sections, _unwrap_sections
-
-
-@pytest.mark.parametrize(
-    "sections",
-    [
-        [
-            "SECTION KEY1=VAL1 KEY2=VALUE2 KEYKEY3=VALUEVALUE3 KEYFOUR=VALUEFOUR KEYFIVEFIVE=VALFIVEFIVE"
-        ],
-        [
-            "NEWSECT KEY1=VAL1 KEY2=VALUE2 KEYKEY3=VALUEVALUE3 KEYFOUR=VALUEFOUR KEYFIVEFIVE=VALFIVEFIVE"
-        ],
-    ],
+from hypothesis import given, strategies as st
+from sodafile.flf.utils import (
+    _wrap_sections,
+    _unwrap_sections,
+    _dict_to_line,
+    _line_to_dict,
 )
-class TestWrapUnwrapSections:
-    def test_wrap_lines(self, sections):
-        print("aaa", sections)
-        text = _wrap_sections(sections, indent=10)
-        for line in text.splitlines(keepends=True):
-            assert len(line) == 80
+from . import strategies as cst
+
+
+@given(cst.unwrapped_sections())
+def test_wrap_line_length(unwrapped_section):
+    text = _wrap_sections([unwrapped_section], indent=cst.INDENT_SIZE)
+    for line in text.splitlines(keepends=True):
+        assert len(line) == 80
+
+
+@given(cst.unwrapped_sections())
+def test_wrap_unwrap(input):
+    input = [input]
+    wrapped = _wrap_sections(input, indent=cst.INDENT_SIZE)
+    unwrapped = _unwrap_sections(wrapped)
+    print("input  =", input)
+    print("output =", unwrapped)
+    assert input == unwrapped
+
+
+@given(st.lists(cst.unwrapped_sections(), min_size=3, max_size=8))
+def test_wrap_unwrap_many(input):
+    wrapped = _wrap_sections(input, indent=cst.INDENT_SIZE)
+    unwrapped = _unwrap_sections(wrapped)
+    print("input  =", input)
+    print("output =", unwrapped)
+    assert input == unwrapped
+
+
+@given(cst.key_val_dict())
+def test_dict_to_line(input):
+    num_kvpairs = len(input)
+    text = _dict_to_line(input)
+    assert len(text.split()) == len(input)
+
+
+@given(cst.key_val_dict())
+def test_dict_to_line_to_dict(input):
+    text = _dict_to_line(input)
+    output = _line_to_dict(text)
+    assert input == output
